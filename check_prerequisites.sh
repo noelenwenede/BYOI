a719bda20fdc777e1e6bb80c36567940db1d7fa1
Jason Hewison <jason.hewison@anaplan.com>
2018-09-28 17:36:44 +0100

    Fixing prerequisites script

--- check_prerequisites.sh
+++ check_prerequisites.sh
@@ -2,7 +2,7 @@
 
 FAILED_PREREQUISITES=0
 
-if ! command -v docker2 >/dev/null 2>&1; then
+if ! command -v docker >/dev/null 2>&1; then
   echo "docker: MISSING" 1>&2
   echo "docker is required to run the development environment" 1>&2
   echo "Please visit https://docs.docker.com/install/ for instructions on how to install it for your platform" 1>&2
@@ -12,7 +12,7 @@ else
   echo "docker: FOUND" 1>&2
 fi
 
-if ! command -v jq2 >/dev/null 2>&1; then
+if ! command -v jq >/dev/null 2>&1; then
   echo "jq: MISSING" 1>&2
   echo "jq is required to run the smoke tests, it's a lightweight tool that helps to parse json in the terminal." 1>&2
   echo "Please visit https://stedolan.github.io/jq/ to download it" 1>&2
