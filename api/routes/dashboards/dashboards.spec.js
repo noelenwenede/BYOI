25cb29c525f7be4d6ee269220bd9da3802d50046
Emmanuel Enwenede <emmanuel@blueeel.co.uk>
2020-08-27 18:14:18 +0100

    updateddashboard test

--- api/routes/dashboards/dashboards.spec.js
+++ api/routes/dashboards/dashboards.spec.js
@@ -5,7 +5,7 @@ const app = require("../../app");
 describe('Dashboards', () => {
 
   it('It should get sample dashboards', async (done) => {
-    const res = await request(app).get('/ping');
+    const res = await request(app).get('/dashboards');
     expect(res.statusCode).toEqual(200);
     done();
   })
